ad6fe9f07a638b8afeb264c95d4b273d9db15d82
Abhi <abhi2bari929@gmail.com>
2024-09-29 11:55:52 +0530

    chnage

--- build/app/tweet/muatations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mutations = void 0;
-exports.mutations = `
-
-        createTweet(payload: CreateTweetData!): Tweet
-
-`;
